eb2b4b0145749ebac83a5817de8d4f7ab0a617a3
MichaelLuuTrong <michaelluutrong@gmail.com>
2025-02-26 18:45:01 -0800

    Added dict_to_sorted_list function to receive list of alphabetical characters sorted by frequency. Added printing of formatted report.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter, character_counter
+from stats import word_counter, character_counter, dict_to_sorted_list
 
 def get_book_text(filepath: str):
     with open(filepath) as f:
@@ -10,9 +10,16 @@ def main():
     if not raw_text:
         print("Error: text could not be read.")
     word_count = word_counter(raw_text)
-    print(f"{word_count} words found in the document")
     character_count = character_counter(raw_text)
-    print(character_count)
-
+    sorted_alpha_count = dict_to_sorted_list(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at /books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for alpha_and_count in sorted_alpha_count:
+        for alpha, count in alpha_and_count.items():
+            print(f"{alpha}: {count}")
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,6 @@
 def word_counter(text: str):
     return len(text.split())
 
-
 def character_counter(text: str):
     lowercase_text= text.lower()
     character_dict = {}
@@ -11,3 +10,28 @@ def character_counter(text: str):
         elif character in character_dict:
             character_dict[character] = character_dict[character] + 1
     return character_dict
+
+def get_value_from_dict_in_list(dict_of_count:dict):
+    return list(dict_of_count.values())[0]
+
+def get_key_from_dict_in_list(dict_of_count: dict):
+    return list(dict_of_count.keys())[0]
+
+def dict_to_sorted_list(dict_of_counts: dict):
+    # dict to unsorted list
+    list_of_dicts = [{character: value} for character, value in dict_of_counts.items()]
+    
+    # sort by value in key-value pair
+    # can use lambda function instead
+    list_of_dicts.sort(key=get_value_from_dict_in_list, reverse = True)
+    
+    # remove non-alpha characters
+    list_of_alphas = []
+    for character_and_count_dict in list_of_dicts:
+        # can use lambda function instead
+        if get_key_from_dict_in_list(character_and_count_dict).isalpha():
+            list_of_alphas.append(character_and_count_dict)
+
+    return list_of_alphas
+
+    
